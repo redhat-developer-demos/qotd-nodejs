a29c4e3d40f824ee77c7b0d8f6a81a9c995e2467
Don Schenck <donschenck@localhost.localdomain>
2020-09-02 14:20:45 -0400

    Added all the quotes

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const app = express();
 
 const port = 10000;
 
+
+quotes = [];
+
+q = { "id":"0", "quotation":"It is not only what you do, but also the attitude you bring to it that makes you a success.","author":"Don Schenck"};
+quotes.push(q);
+q = { "id":"1", "quotation":"Knowledge is power.","author":"Francis Bacon"};
+quotes.push(q);
+q = { "id":"2", "quotation":"Life is really simple, but we insist on making it complicated.","author":"Confucius"};
+quotes.push(q);
+q = { "id":"3", "quotation":"This above all, to thine own self be true.","author":"William Shakespeare"};
+quotes.push(q);
+q = { "id":"4", "quotation":"Never complain. Never explain.","author":"Katharine Hepburn"};
+quotes.push(q);
+q = { "id":"5", "quotation":"Do be do be dooo.","author":"Frank Sinatra"};
+quotes.push(q);
+
+
 app.get('/', (req, res) => {
     res.send('qotd');
 });
@@ -14,6 +31,15 @@ app.get('/version', (req, res) => {
 
 app.get('/writtenin', (req, res) => {
     res.send('Node.js');
+});
+
+app.get('/quotes', (req, res) => {
+    res.send(quotes);
+});
+
+app.get('/quotes/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(quotes[id]);
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
